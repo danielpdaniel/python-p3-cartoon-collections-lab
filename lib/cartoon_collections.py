e4985fb8e98f5c2f412d7e7dcdddda8b25da083e
danielpdaniel <artofjquinn@gmail.com>
2023-06-28 00:26:05 -0400

    Update functions to get tests passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    length = len(dwarves)
+    for n in range(0,length):
+        print(f"{n+1}. {dwarves[n]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [f"{call.title()}!" for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+        else:
+            pass
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strs):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for str in strs:
+        if str in cheeses:
+            return str
+    return None
